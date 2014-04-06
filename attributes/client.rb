94699c05a9c74492f5e47b987fbb13a406cdd648
Andy Cunningham <andycunn@gmail.com>
2014-04-05 20:23:14 -0700

    update to percona 5.6

--- attributes/client.rb
+++ attributes/client.rb
@@ -1,6 +1,6 @@
 case node["platform_family"]
 when "debian"
-  normal['mysql']['client']['packages'] = %w{libmysqlclient-dev percona-server-client-5.5}
+  normal['mysql']['client']['packages'] = %w{libmysqlclient-dev percona-server-client-5.6}
 when "rhel"
-  normal['mysql']['client']['packages'] = %w{Percona-Server-client-55}
+  normal['mysql']['client']['packages'] = %w{Percona-Server-client-56}
 end
